e669f6fcba0a70527e8099bc9acaebfe069b4a4d
laurirasanen <laurirasanen@occasionally.cool>
2019-03-26 02:55:02 +0200

    Fix typo in setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,9 @@ from setuptools import setup
 
 setup(
     name="jtimer-api",
-    install_requires={
-        "doc": [
-            "sphinxcontrib.httpdomain",
-            "sphinxcontrib.autohttp.flask",
-            "sphinxcontrib.autohttp.flaskqref",
-        ]
-    },
+    install_requires=[
+        "sphinxcontrib.httpdomain",
+        "sphinxcontrib.autohttp.flask",
+        "sphinxcontrib.autohttp.flaskqref",
+    ],
 )
