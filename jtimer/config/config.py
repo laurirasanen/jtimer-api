accc28b954dc2ccd5d286f27df23aadbc1cfb7ea
laurirasanen <laurirasanen@occasionally.cool>
2019-03-26 03:15:27 +0200

    Fix docs build error when missing environment variables

--- jtimer/config/config.py
+++ jtimer/config/config.py
@@ -2,10 +2,27 @@ import os
 
 
 class MySQL(object):
-    if os.environ.get("READTHEDOCS") is True:
-        return
-        
-    MYSQL_HOST = os.environ["RDS_HOSTNAME"]
-    MYSQL_PORT = int(os.environ["RDS_PORT"])
-    MYSQL_USER = os.environ["RDS_USERNAME"]
-    MYSQL_PASSWORD = os.environ["RDS_PASSWORD"]
+    # make sure variables are set to something,
+    # so we don't get errors while building docs
+    if os.environ.get("RDS_HOSTNAME") is not None:
+        MYSQL_HOST = os.environ.get("RDS_HOSTNAME")
+    else:
+        MYSQL_HOST = "localhost"
+
+    if os.environ.get("RDS_PORT") is not None:
+        MYSQL_PORT = int(os.environ.get("RDS_PORT"))
+    else:
+        MYSQL_PORT = 3306
+
+    if os.environ.get("RDS_USERNAME") is not None:
+        MYSQL_USER = os.environ.get("RDS_USERNAME")
+    else:
+        MYSQL_USER = "jtimer"
+
+    if os.environ.get("RDS_PASSWORD") is not None:
+        MYSQL_PASSWORD = os.environ.get("RDS_PASSWORD")
+    else:
+        MYSQL_PASSWORD = "foo"
+
+    if os.environ.get("RDS_DB_NAME") is None:
+        os.environ["RDS_DB_NAME"] = "jtimerdb"
