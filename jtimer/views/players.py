ef7ced2ce90faec2d08fb0e4b7503b7b60445c91
laurirasanen <laurirasanen@occasionally.cool>
2019-03-26 02:29:42 +0200

    Add sphinx docs

--- jtimer/views/players.py
+++ jtimer/views/players.py
@@ -8,10 +8,54 @@ from jtimer.models.player import (
 
 @players_index.route("/list")
 def list_players():
+    """Return a list of players.
+
+    .. :quickref: Player; Get a list of players.
+
+    **Example request**:
+
+    .. sourcecode:: http
+
+      GET /players/list?limit=2 HTTP/1.1
+
+    **Example response**:
+
+    .. sourcecode:: json
+    
+      [
+          {
+              "id": 1,
+              "name": "Larry",
+              "rank_info": {
+                  "demo_points": 0,
+                  "demo_rank": 0,
+                  "soldier_points": 0,
+                  "soldier_rank": 0
+              },
+              "steamid": "STEAM_1:1:50152141"
+          },
+          {
+              "id": 2,
+              "name": "kaptain",
+              "rank_info": {
+                  "demo_points": 0,
+                  "demo_rank": 0,
+                  "soldier_points": 0,
+                  "soldier_rank": 0
+              },
+              "steamid": "STEAM_0:0:36730682"
+          }
+      ]
+    
+    :query limit: amount of players to get. (default: 50, min: 1, max: 50)
+    :query start: list index to start getting players from. (default: 0, min: 0)
+    :status 200: players found.
+    :returns: Player
+    """
     limit = request.args.get("limit", default=50, type=int)
     start = request.args.get("start", default=0, type=int)
 
-    limit = max(0, min(limit, 50))
+    limit = max(1, min(limit, 50))
     start = max(0, start)
 
     players = mdl_list_players(limit=limit, start=start)
@@ -26,6 +70,43 @@ def list_players():
 
 @players_index.route("/search")
 def find_player():
+    """Search for a player.
+
+    .. :quickref: Player; Find a player.
+
+    **Example request**:
+
+    .. sourcecode:: http
+    
+      GET /players/search?playerid=1 HTTP/1.1
+    
+    **Example response**:
+
+    .. sourcecode:: json
+    
+      {
+          "id": 1,
+          "name": "Larry",
+          "rank_info": {
+              "demo_points": 0,
+              "demo_rank": 0,
+              "soldier_points": 0,
+              "soldier_rank": 0
+          },
+          "steamid": "STEAM_1:1:50152141"
+      }
+    
+    :query playerid: the playerid to search for.
+    :query steamid: the steamid to search for.
+    :query name: the name to search for.
+
+    **Note**: If multiple parameters are supplied, only one of them will be used.
+    Parameters are prioritized in the order: playerid > steamid > name.
+    
+    :status 200: player found.
+    :status 204: no player found.
+    :returns: Player
+    """
     playerid = request.args.get("playerid", default=None, type=int)
     steamid = request.args.get("steamid", default=None, type=str)
     name = request.args.get("name", default=None, type=str)
