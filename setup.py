08d8d4e5a61de350b49a60a966f6ad81cee025b8
laurirasanen <laurirasanen@occasionally.cool>
2019-03-26 02:57:12 +0200

    Fix incorrect package names in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
 from setuptools import setup
 
 
-setup(
-    name="jtimer-api",
-    install_requires=[
-        "sphinxcontrib.httpdomain",
-        "sphinxcontrib.autohttp.flask",
-        "sphinxcontrib.autohttp.flaskqref",
-    ],
-)
+setup(name="jtimer-api", install_requires=["sphinxcontrib-httpdomain"])
