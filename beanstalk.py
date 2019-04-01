8cb61c175c670d208a819be80ae30cab764624c5
laurirasanen <laurirasanen@occasionally.cool>
2019-04-01 15:05:33 +0300

    Fix zip archive script

--- beanstalk.py
+++ beanstalk.py
@@ -1,5 +1,6 @@
 """Zip up project for AWS Elastic Beanstalk deployment"""
 
+import os
 import zipfile
 from configparser import ConfigParser
 
@@ -9,10 +10,26 @@ version = config.get("root", "version", fallback=None)
 
 files = ["application.py", "setup.py", "requirements.txt", "jtimer"]
 
-output = f"jtimer-{version}.zip"
+output = f"jtimer-api-{version}.zip"
+
+
+def zipdir(path, zip_handle):
+    """Recursively add directory to zip archive"""
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            path = os.path.join(root, file)
+            # ignore pyc
+            if any(word in path for word in ["__pycache__", ".pyc"]):
+                continue
+            else:
+                zip_handle.write(path)
+
 
 if __name__ == "__main__":
     z = zipfile.ZipFile(output, mode="x")
     for f in files:
-        z.write(f)
+        if os.path.isdir(f):
+            zipdir(f, z)
+        else:
+            z.write(f)
     z.close()
